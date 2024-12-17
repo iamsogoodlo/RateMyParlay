6076df989cd4fd5438dd3ffe47259da06e5bc477
iamsogoodlo <101160255+iamsogoodlo@users.noreply.github.com>
2024-12-16 21:23:47 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -256,36 +256,3 @@ def rate_leg(times_true):
         return "Good"
     return "Bad"
 
-# Main script
-while True:
-    try:
-        first_name = input("Player first name: ")
-        last_name = input("Player last name: ")
-        name = player_name(first_name, last_name)
-        url = create_url_player(name)
-        df = scrape_player_stats(url)
-        break
-    except Exception as e:
-        print(f"Error fetching player stats: {e}. Please try again.")
-
-while True:
-    try:
-        bet_type = input("What is your bet type: ").lower()
-        times_true = take_leg(bet_type, df)
-        if times_true is not None:
-            leg_rating = rate_leg(times_true)
-            print(f"Leg rating: {leg_rating}")
-            break
-    except Exception as e:
-        print(f"Error evaluating leg: {e}. Please try again.")
-
-while True:
-    try:
-        odds = input("Odds of bet (+/-): ")
-        amt_wagered = input("How much did you wager: ")
-        ev = calculate_expected_value(odds, amt_wagered)
-        if ev:
-            print("Expected Value Calculation:", ev)
-            break
-    except Exception as e:
-        print(f"Error calculating expected value: {e}. Please try again.")
